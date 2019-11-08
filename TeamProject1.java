d0d688aad68e9a1e02876ee2ce996d41f4c5928b
AM~G <50150879+AmirrezaG@users.noreply.github.com>
2019-11-09 00:46:39 +0330

    Update TeamProject1.java

--- TeamProject1.java
+++ TeamProject1.java
@@ -14,7 +14,7 @@ public class TeamProject1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hey");
         // TODO code application logic here
     }
     
